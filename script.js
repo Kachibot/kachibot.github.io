441ff782c6b5e2ee1e72748fa49fa3c9d4537909
Kachibot <84442448+Kachibot@users.noreply.github.com>
2021-05-19 10:49:20 +0200

    upload

---
+++ script.js
@@ -0,0 +1,9 @@
+history.scrollRestoration = 'manual';
+
+if (history.scrollRestoration) {
+    history.scrollRestoration = 'manual';
+} else {
+    window.onbeforeunload = function () {
+        window.scrollTo(0, 0);
+    }
+}
